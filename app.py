15e96f8a5978054a70a3f4105b6f8c55039ad09d
lucastanisci01 <lucastanisci01@gmail.com>
2026-09-18 10:41:45 +0200

    app.py

--- app.py
+++ app.py
@@ -13,12 +13,16 @@ with st.expander("❓ Come scaricare i file JSON da Instagram"):
     st.markdown("""
     Per usare questa app, devi richiedere i tuoi dati ufficiali a Instagram. Segui questi passaggi dal tuo telefono:
     1. Apri l'app di Instagram e vai sul tuo profilo.
-    2. Tocca il menu in alto a destra (le tre linee) e vai su **La tua attività**.
-    3. Scorri fino in fondo e tocca **Scarica le tue informazioni**.
-    4. Tocca **Scarica o trasferisci informazioni**, seleziona il tuo profilo Instagram e scegli **Alcune delle tue informazioni**.
-    5. Scorri fino alla sezione *Connessioni*, seleziona **Follower e seguiti** e vai avanti.
-    6. Seleziona *Scarica sul dispositivo*. **IMPORTANTE:** cambia il formato da HTML a **JSON** e l'intervallo di date in **Dall'inizio**.
-    7. Tocca **Crea file**.
+    2. Tocca il menu in alto a destra (le tre linee) e vai su **Centro gestione account**.
+    3. Scorri fino a trovare **Le tue informazioni e autorizzazioni**.
+    4. Tocca **Esporta le tue informazioni** e poi **Crea esportazione** e seleziona il tuo profilo Instagram.
+    5. Tocca **Esporta sul dispositivo**.
+    6. Vai su **Ricevi una notifica** e inserisci la mail.
+    7. Vai su **Personalizza informazioni**, seleziona in **Contatti** la voce **Follower e persone/Pagine seguite** e salva.
+    8. Vai su **Intervallo di date**, seleziona **Sempre** e salva.
+    9. Vai su **Formato**, seleziona **JSON** e salva.
+    10. A questo punto tocca *Avvia esportazione*.
+   
     
     *Instagram ti invierà un'email (di solito entro pochi minuti). Scarica il file ZIP, estrailo, e troverai i due file che ti servono nella cartella `connections/followers_and_following`.*
     """)
